3cb4cbfccb83581fd4f63fd4e91367cefe5e2957
Cameron Wallace <cameron.wallace@metergroup.com>
2023-06-12 13:46:48 -0700

    update header spacing

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -14,9 +14,9 @@ class NavBar extends React.Component {
                 <div id="header-grid">
                     <nav>
                         <ul id="menulink">
-                            <Link className="navbutton" to="/">CW</Link>
+                            <Link id="navbutton-name" to="/">cw</Link>
                             {/* <Link className="navbutton" to="/about">about</Link> */}
-                            <Link className="navbutton" to="/goals">goals</Link>
+                            {/* <Link className="navbutton" to="/goals">goals</Link> */}
                             <Link className="navbutton" to="/trips">trips</Link>
                             <Link className="navbutton" to="/projects">projects</Link>
                         </ul>
