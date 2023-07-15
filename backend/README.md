16ff8c0dc96619bc331c7d7f626db92ba15c9436
cameronCJW <cmrnwllc@gmail.com>
2023-07-15 11:10:42 -0700

    Change goals to checks

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,7 @@
 ## Getting on to cPanel
+
+1. Pull the changes into the `/personalsite/` folder using the git helper.
+2. Copy `/personalsite/backend/` to `/backend/`
+3. Go to Setup Python App => backend => Configuration files => run requirements.txt
+4. add `keys.py` to `/backend/`
+5. run `python manage.py migrate`
--- frontend/src/goals.js
+++ frontend/src/goals.js
@@ -9,8 +9,11 @@ const Goals = function(props) {
       <main>
         <div id="goals-header">
           <h2>Goals</h2>
-          <li className="goal-bullet"> 1/3 - Climb three volcanoes before the end of 2023 </li>
-          <li className="goal-bullet-indented"> 6/11/23 - Mt. St. Helens </li>
+          <li className="goal-bullet"><input type="checkbox" checked={false}/> 1/3 - Climb three volcanoes before the end of 2023 </li>
+          <li className="goal-bullet-indented"><input type="checkbox" checked={true}/> 6/11/23 - Mt. St. Helens </li>
+          <li className="goal-bullet-indented"><input type="checkbox" checked={false}/></li>
+          <li className="goal-bullet-indented"><input type="checkbox" checked={false}/></li>
+          <li className="goal-bullet"><input type="checkbox" checked={false}/> Start a new full time job </li>
         </div>
       </main>
     </>
